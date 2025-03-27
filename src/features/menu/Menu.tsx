dfe6013ab8f233aa5e15dc30a77e463f973cc03d
Hazar <hazarnamdarrr@gmail.com>
2025-03-28 00:34:40 +0300

    feature(menuLoader): Implemented a menu loader by using the react router loader logic

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,5 +1,21 @@
+import IPizza from '@interfaces/IPizza';
+import { getMenu } from '@services/apiRestaurant';
+import { useLoaderData } from 'react-router-dom';
+import MenuItem from './MenuItem';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData() as Array<IPizza>;
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
+
+// React Router loader function
+export const loader = async () => await getMenu();
 
 export default Menu;
