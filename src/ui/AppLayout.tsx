1bb9c0d64a7dfe5b205f7b403c1c8cd7f131df3c
Hazar <hazarnamdarrr@gmail.com>
2025-03-29 01:41:16 +0300

    feature(Loader): Implemented a loading... feature for the menu fetching

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,11 +1,18 @@
+import { Outlet, useNavigation } from 'react-router-dom';
+
 import CartOverview from '@features/cart/CartOverview';
 
 import Header from '@ui/Header';
-import { Outlet } from 'react-router-dom';
+import Loader from '@ui/Loader';
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
