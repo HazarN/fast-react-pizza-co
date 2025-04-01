2774f25f6492cd1a382682637206a11b7c75b45d
HazarN <hazar@smartystudios.com>
2025-04-02 02:41:40 +0300

    feature(ErrorPage): Error page styling is added to the project

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -6,12 +6,17 @@ function Error() {
   const error = useRouteError() as { data?: string; message?: string };
 
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <div className='w-10/12 h-1/5 bg-stone-200 rounded-md py-6 px-8 flex flex-col justify-between'>
+        <div>
+          <h1 className='text-xl text-center font-semibold mb-2'>Something went wrong 😢</h1>
+          <p>{error.data || error.message}</p>
+        </div>
 
-      <p>{error.data || error.message}</p>
-
-      <LinkButton to={'-1'}>&larr; Go back</LinkButton>
+        <div className='flex flex-1'>
+          <LinkButton to={'-1'}>&larr; Go back</LinkButton>
+        </div>
+      </div>
     </div>
   );
 }
